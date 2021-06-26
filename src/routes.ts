b3ebae296be53ba76893cc45f5ceb838e0b8ddd2
marceloricardo777 <marceloricardoo7777@gmail.com>
2021-06-26 14:20:52 -0300

    route init error

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Response, Router } from "express";
+import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from './middlewares/ensureAdmin';
@@ -26,7 +26,7 @@ router.post('/complements', ensureAuthenticated, createComplimentController.hand
 router.get('/users/complements/send', ensureAuthenticated, listUserSenderController.handle);
 router.get('/users/complements/receiver', ensureAuthenticated, listUserReceiverController.handle);
 router.get('/tags', ensureAuthenticated, listTagsController.handle);
-router.get('/', (response: Response) => {
+router.get('/', (request, response) => {
     return response.json('Welcome to API')
 });
 
